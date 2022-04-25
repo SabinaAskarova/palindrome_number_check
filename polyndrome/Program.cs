01107c9d4d274783bd1ec1254b44129f8e8f67b7
SebineEskerova SabinaAskarova <sabinaaskarova56@gmail.com>
2022-04-25 23:01:51 +0400

    finish

---
+++ polyndrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace polyndrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int number = Int32.Parse( Console.ReadLine() );
+            int mainnumber = number;
+            int reversenum=0;
+            while (number>0)
+            {
+                reversenum = reversenum*10+ number % 10;
+                number = number / 10;
+            }
+            if (mainnumber==reversenum)
+            {
+                Console.WriteLine("Polindrome ededdir.");
+
+            }
+            else
+            {
+                Console.WriteLine("Polindrome eded deyil.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
